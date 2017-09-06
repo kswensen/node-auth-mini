bdedbea2f933785539ecee91c51e16fdc16b2707
StayinNasty <keganswensen@gmail.com>
2017-09-06 12:00:23 -0600

    Got it working

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy= require(`${__dirname}/strategy.js`);
 
 const app = express();
+app.use(session({
+    secret: 'A suh dude',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
 
-const port = 3000;
+passport.serializeUser((user, done) => {
+    done(null, {id: user.id, display: user.displayName, nickname: user.nickname, email: user.emails[0].value});
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+app.get('/login', passport.authenticate('auth0', {successRedirect: '/me', failureRedirect: '/login', failureFlash: true}));
+app.get('/me', (req, res) => {
+    if(!req.user){
+        res.redirect('/login');
+    } else {
+        res.status(200).send(JSON.stringify(req.user, null, 10));
+    }
+})
+
+const port = 3001;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
